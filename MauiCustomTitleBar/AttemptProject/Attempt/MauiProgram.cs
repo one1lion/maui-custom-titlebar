15a6bc7eb4546823fb8f587fbd0d08655a51b0e8
one1lion <i_am_listening@hotmail.com>
2022-07-28 20:32:04 -0600

    Prepared attempt project (renamed projects and folders to shorten the paths)

---
+++ MauiCustomTitleBar/AttemptProject/Attempt/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using MauiCustomTitleBar.Shared.ServiceInterfaces;
+using MauiCustomTitleBar.Shared.Services;
+
+namespace MauiCustomTitleBar.CustomizeAttempt;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+        builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
+
+        return builder.Build();
+    }
+}
